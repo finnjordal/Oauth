f8d51a708d5dae2be26c6795c24fc47785becb24
Finn Jordal <finn@jordal.dk>
2012-10-25 15:16:52 +0200

    Start på website

---
+++ GoogleInstalledApplication/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Web;
+using System.Net.Http;
+using System.Json;
+
+namespace GoogleInstalledApplication
+{
+  public partial class Form1 : Form
+  {
+    public Form1()
+    {
+      InitializeComponent();
+    }
+
+    private void button1_Click(object sender, EventArgs e)
+    {
+      webBrowser1.Url = new Uri("https://accounts.google.com/o/oauth2/auth?client_id=185454440685-6dfl2dqdonfcsstooncf7cps6qt2vs4p.apps.googleusercontent.com&scope=https://www.googleapis.com/auth/drive&response_type=code&redirect_uri=http://localhost");
+    }
+
+    private async void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+    {      
+      if (e.Url.Query.Contains("code="))
+      {
+        NameValueCollection coll = HttpUtility.ParseQueryString(e.Url.Query);
+        string code = coll["code"];
+        string error = coll["error"];
+        if (!String.IsNullOrEmpty(error))
+        {
+          MessageBox.Show("error: " + error);
+        }
+        else if (!String.IsNullOrEmpty(code))
+        {
+          MessageBox.Show("code: " + code);
+
+          HttpClient client = new HttpClient();
+          var fueColl = new Dictionary<string, string>();
+          fueColl["client_id"] = "185454440685-6dfl2dqdonfcsstooncf7cps6qt2vs4p.apps.googleusercontent.com";
+          fueColl["client_secret"] = "Xz4cQUFPX_feSETa3OWF_jSH";
+          fueColl["code"] = code;
+          fueColl["grant_type"] = "authorization_code";
+          fueColl["redirect_uri"] = "http://localhost"; //"urn:ietf:wg:oauth:2.0:oob";
+          HttpContent content = new FormUrlEncodedContent(fueColl);
+          HttpResponseMessage response = await client.PostAsync("https://accounts.google.com/o/oauth2/token", content);
+          bool statusOk = response.IsSuccessStatusCode;
+          string responseBody = await response.Content.ReadAsStringAsync();
+          MessageBox.Show("response: " + responseBody);
+          JsonObject accessInfo = (JsonObject)JsonObject.Parse(responseBody);
+          string accessToken = (string)accessInfo["access_token"];
+          MessageBox.Show("access_token: " + accessToken);
+
+          client.DefaultRequestHeaders.Add("Authorization", "Bearer " + accessToken);
+          response = await client.GetAsync("https://www.googleapis.com/drive/v2/files");
+          statusOk = response.IsSuccessStatusCode;
+          string filesstring = await response.Content.ReadAsStringAsync();
+          JsonObject files = (JsonObject)JsonObject.Parse(filesstring);
+          JsonArray filelist = (JsonArray)files["items"];
+          foreach (JsonObject file in filelist)
+          {
+            MessageBox.Show(file["title"].ToString());
+          }
+        }
+        else
+        {
+          MessageBox.Show("Ukendt request");
+        }
+      }
+    }
+  }
+}
---
+++ OAuth/default.aspx
@@ -0,0 +1,51 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/site.Master" AutoEventWireup="true"
+  CodeBehind="default.aspx.cs" Inherits="OAuth._default" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+  <header id="home" class="jumbotron subhead">
+    <div class="page-header">
+      <h1>
+        Introduktion <small>Hvad er BAPI?</small></h1>
+    </div>
+  </header>
+  <section>
+    <div class='row'>
+      <div class="span8">
+        <p>
+          DB2 Blob web API (BAPI) gør det enkelt og effektivt at læse og skrive blobs til
+          DB2 på mainframe fra andre platforme. Blobs er typisk dokumenter af forskellige
+          typer, som f.eks. Word, Excel, og foto. Platformen, som BAPI kan anvendes fra vil
+          i vores situation være en .NET platform, men det kunne lige så godt være en anden,
+          da BAPI er baseret på standarder.
+        </p>
+        <p>
+          Du kan læse mere om web API programmeringsinterfacet på <a href="webapi.aspx">Web API-siden</a>.
+          Kodeeksempler på brugen af BAPI kan du finde på <a href="kodeeksempler.aspx">Kodeeksempel-siden</a>.
+          Her finder du også et <a href="download/DB2BlobsAPIKlient.zip">Visual Studio 2010 projekt, som anvender alle facetter af BAPI</a>.
+        </p>
+        <h2>
+          Formål</h2>
+        <p>
+          Formålet med BAPI er flerhovedet:</p>
+        <ul>
+          <li>I forbindelse med undersøgelsen af hvorvidt DB2 Connect skal være en del af .NET
+            TDM'erne, er det vigtigt at vurdere DB2 Connect ud fra praktiske erfaringer med
+            produktet. BAPI er lavet for at få disse erfaringer.</li>
+          <li>I forbindelse med indføringen af REST baserede web services(web API'er) som alternativ
+            til de SOAP baserede i .NET TDM'erne er det væsentligt at få praktiske erfaringer,
+            således at behovet for infrastruktur og retningslinjer kan vurderes. BAPI er lavet
+            som en del af dette arbejde</li>
+          <li>Forretningsområderne har ytret ønske om at kunne anvende en anden mekanisme end
+            KWS/Zsrør til at læse og skrive blobs til DB2 på mainframen. BAPI kan ses som en
+            prototype på en sådan mekanisme, som kan anvendes til at få viden om fordele og
+            ulemper ved et sådant alternativ til KWS/Zsrørs blobsupport. </li>
+        </ul>
+      </div>
+    </div>
+  </section>
+  <script type="text/javascript">
+    $('#topbar li').removeClass('active');
+    var a = $("#topbar li a[href='default.aspx']")[0];
+    var li = $(a).parent().addClass('active');
+  </script>
+</asp:Content>
